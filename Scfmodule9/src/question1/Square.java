a4756f5d5b23f095a6482c101dee8887361725b6
meta-ajay-shringi <52688974+meta-ajay-shringi@users.noreply.github.com>
2019-07-29 18:53:25 +0530

    Update Square.java

--- Scfmodule9/src/question1/Square.java
+++ Scfmodule9/src/question1/Square.java
@@ -45,10 +45,10 @@ public class Square implements Shape
     	return date;
     }
     
-	/*
-	    * This method is used to calculate origin point.
-	    * @return This return origin point.
-	    */
+     /*
+      * This method is used to calculate origin point.
+      * @return This return origin point.
+      */
     public Point getOrigin() {
         return originPoint;
     }
@@ -59,35 +59,35 @@ public class Square implements Shape
      * @param givenPoint, This is point to be check.
      * @return This return true is point inside else return false.
      */
-	public boolean isPointEnclosed(Point givenPoint) {
+      public boolean isPointEnclosed(Point givenPoint) {
 		if(givenPoint.x > originPoint.x && givenPoint.x < maxSquarePoint.x && givenPoint.y > originPoint.y && givenPoint.y < maxSquarePoint.y) {
 			return true;
 		}
 		return false;
 	}
 	
-	   /*
-	    * This method is used to get shape type.
-	    * @return This return the type of shape.
-	    */
+      /*
+       * This method is used to get shape type.
+       * @return This return the type of shape.
+       */
 	public String getShapeType() {
 		return "SQUARE";
 	}
 
 	/*
-	    * This method is used to calculate distance between (0,0) to shape origin point.
-	    * @return, This return distance between (0,0) to shape origin point.
-	    */
-	public double getOriginDistance() {
+	 * This method is used to calculate distance between (0,0) to shape origin point.
+	 * @return, This return distance between (0,0) to shape origin point.
+	 */
+	 public double getOriginDistance() {
 		
 		return Math.sqrt((originPoint.x - 0) * (originPoint.x - 0) + (originPoint.y - 0) * (originPoint.y - 0));
 	}
 	
-	/*
+     /*
      * This method is used to get maximum point in square. 
      * @return diagonally opposite points of square.
      */
-    private Point getMaxSquarePoint() {
+      private Point getMaxSquarePoint() {
         return new Point(originPoint.x + width, originPoint.y + width);
     }
     
