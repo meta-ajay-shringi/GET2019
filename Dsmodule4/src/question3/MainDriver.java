a31335ccabf93a1868cecb61211fedae20eb10a5
meta-ajay-shringi <ajay.shringi@metacube.com>
2019-08-05 18:39:52 +0530

    Ds session4 assignment initial commit

---
+++ Dsmodule4/src/question3/MainDriver.java
@@ -0,0 +1,20 @@
+package question3;
+
+import java.util.*;
+
+/**
+ * This is the class containing main method
+ */
+public class MainDriver {
+
+	public static void main(String[] args) {
+
+		Scanner inputObj = new Scanner(System.in);
+		MolecularMass mass = new MolecularMass();
+		String compound;
+		System.out.print("enter the compound formula : ");
+		compound = inputObj.nextLine();
+		System.out.println("Molecular mass of compound : " + mass.calculateMass(compound.toUpperCase()));
+	}
+
+}
