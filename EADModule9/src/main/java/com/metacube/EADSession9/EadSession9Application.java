d76a1bae12f2df4c3bf0f88094c6d47b9ea60ff8
meta-ajay-shringi <ajay.shringi@metacube.com>
2019-09-23 11:39:35 +0530

    EAD session9 assignment initial commit

---
+++ EADModule9/src/main/java/com/metacube/EADSession9/EadSession9Application.java
@@ -0,0 +1,16 @@
+package com.metacube.EADSession9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * main class for spring application
+ */
+@SpringBootApplication
+public class EadSession9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EadSession9Application.class, args);
+	}
+
+}
---
+++ EADModule9/src/main/java/com/metacube/EADSession9/model/BaseModel.java
@@ -0,0 +1,5 @@
+package com.metacube.EADSession9.model;
+
+public class BaseModel {
+
+}
